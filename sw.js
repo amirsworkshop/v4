9784124d71797cd5b4e214bc0588828c98b3b020
somayehgh <somayehghazvinian@gmail.com>
2020-09-30 20:40:01 -0700

    Updates

--- sw.js
+++ sw.js
@@ -27,7 +27,7 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-b892c879c93986dc4690.js"
+    "url": "webpack-runtime-d9f68613e9831c89f4c9.js"
   },
   {
     "url": "framework-3321703c33570853116c.js"
@@ -37,7 +37,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "5e47e02295882560922654b510a01e08"
+    "revision": "c148bbb48c5515f00ef21a4c42a527c2"
   },
   {
     "url": "component---cache-caches-gatsby-plugin-offline-app-shell-js-b1462017a2c34a95e2b4.js"
@@ -48,7 +48,7 @@ self.__precacheManifest = [
   },
   {
     "url": "page-data/app-data.json",
-    "revision": "e699e4304eac4b3dbb4e8c72a0431df4"
+    "revision": "27d6a723563659b1aec9861c23b387c9"
   },
   {
     "url": "manifest.webmanifest",
